a0d469c6f1b23cf53e7df5ec5ee64236abbcd50c
zetsubo06 <134783472+zetsubo06@users.noreply.github.com>
2023-07-12 09:44:56 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+
+const personagens = document.querySelectorAll('.personagem');
+console.log('personagens');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth <450){
+            window.scrollTo({top: 0, behavior: 'smooth' });
+        }
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+
+        personagem.classList.add('selecionado');
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        //passo 2 - alterar a imagem grande 
+        const idPersonagem = personagem.attributes.id.value;
+
+        imagemPersonagemGrande.src = `./src/imagens/imagens/card-${idPersonagem}.png`;
+     
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute ("data-name");
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem')
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description')
+    })
+})
